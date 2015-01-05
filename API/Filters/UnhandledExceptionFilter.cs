4c1009ddbe30c2a68747708e43438c71f9160669
jamesrichford <jamesrichford@googlemail.com>
2015-01-05 22:53:07 +0000

    Add API Exception Filter

---
+++ API/Filters/UnhandledExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace Sett.API.Filters
+{
+    public class UnhandledExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            response.Content = new StringContent(context.Exception.Message);
+            context.Response = response;
+        }
+    }
+}
